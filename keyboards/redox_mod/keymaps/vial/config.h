31a8bca41f6c0eb7d603c7e04392263ed8e0785b
Dicko Finus <dickofn@gmail.com>
2023-03-08 18:02:40 +0700

    feat: vial support

---
+++ keyboards/redox_mod/keymaps/vial/config.h
@@ -0,0 +1,48 @@
+/*
+Copyright 2018 Mattia Dal Ben <matthewdibi@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x43, 0x06, 0xEA, 0x1E, 0xEB, 0x0E, 0xEB, 0x47}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+/* Select hand configuration */
+#define MASTER_LEFT
+
+#define EE_HANDS
+
+#define USE_SERIAL_PD2
+#define SPLIT_USB_DETECT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_TWINKLE
+
+#define RGBLED_NUM 14
+
